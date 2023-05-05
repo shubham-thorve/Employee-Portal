422336f3aefdc4981a712b3a495d863bb7b1a2c1
swati_kushwaha <swati_kushwaha@persistent.com>
2023-05-05 18:51:44 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './index.css';
+import Home from './routes/Home';
+import {Route, Routes} from 'react-router-dom';
+import Resume from './routes/Resume';
+import Todo from './routes/Todo';
+import Login from './routes/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/resume" element={<Resume/>}/>
+      <Route path="/todo" element={<Todo/>}/>
+      <Route path="/login" element={<Login/>}/>
+    </Routes>
+    </>
   );
 }
 
