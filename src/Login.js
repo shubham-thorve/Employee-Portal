4d3416a091a42842b3d1931fc59395be843baea3
swatikushwaha10 <100906726+swatikushwaha10@users.noreply.github.com>
2023-05-05 19:09:22 +0530

    Add files via upload

---
+++ src/Login.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+// import Navbar from '../components/Navbar';
+// import Footer from '../components/Footer';
+
+const Login = () => {
+  return (
+    <div>
+      {/* <Navbar/> */}
+      <h1>First Login Please.</h1>
+      {/* <Footer/> */}
+    </div>
+  )
+}
+
+export default Login
---
+++ src/Resume.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Resume = () => {
+  return (
+    <div>
+      project
+    </div>
+  )
+}
+
+export default Resume
